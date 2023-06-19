07c08297148dcd6cde69b11869e36ccdbfad4f03
Nicolas9512 <130517472+Nicolas9512@users.noreply.github.com>
2023-06-19 14:21:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+console.log("calculadora")
+
+
+let paso = prompt("elige el paso que deseas, \n 1-suma \n 2-resta \n 3-multiplicacion \n 4-division")
+let A = prompt ("acciona el primer numero del paso")
+let B = prompt ("acciona el segundo numero del paso")
+
+while (paso !=4){
+
+    switch (paso){
+
+        case 1:
+        console.log(A + B)
+        break;
+
+      case 2:
+      console.log(A - B)
+      break;
+
+      case 3:
+        console.log(A * B)
+        break;
+
+        case 4:
+            console.log(A / B)
+            break;
+
+    }
+
+}
+
+const array = [1,2,3,4,5,6];
+let total =0;
+
+const recorrerArray = (Array, fn) =>{
+for(let A=0; i < Array.length; A++){
+
+  function Array ([A]);
+}
+}
+
+function acumular(num){
+
+  total=total + num
+}
+
+recorrerArray(Array,acumular);
+console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
